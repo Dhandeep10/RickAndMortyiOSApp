357767df8227386410ea068cababfa14d39b3908
Dhandeep <dd11singh10@gmail.com>
2023-12-17 22:31:35 +0530

    part 3

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Dhandeep  Singh on 17/12/23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
